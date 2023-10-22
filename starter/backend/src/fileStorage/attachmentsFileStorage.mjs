5453df2fbe451a848731ab0d8d6a7a4f27d850ab
thachvu2410 <quangthach31@gmail.com>
2023-10-22 21:17:47 +0700

    update x-ray for s3-client

--- starter/backend/src/fileStorage/attachmentsFileStorage.mjs
+++ starter/backend/src/fileStorage/attachmentsFileStorage.mjs
@@ -1,8 +1,10 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import AWSXRay from 'aws-xray-sdk-core'
 
 const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
-const s3Client = new S3Client()
+const s3Client = AWSXRay.captureAWSv3Client(new S3Client({}));
+// const s3Client = new S3Client()
 const s3BucketName = process.env.ATTACHMENTS_S3_BUCKET
 
 function generateAttachmentPresignedUrl(todoId) {
